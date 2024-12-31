073e051a589a6c2b5fdc0c0f16e0f608ef50d274
Erdem <rifaterdemsahin@gmail.com>
2024-12-31 15:29:44 +0000

    semblance_ood and service ip

--- 6_Symbols/2_Grafana/render_dashboard.sh
+++ 6_Symbols/2_Grafana/render_dashboard.sh
@@ -4,7 +4,8 @@
 dashboardId="ee8l7vngt74lcc"
 tokenId="glsa_A5cBg7AnfN31RcsUnYRY4ZOVYfTsKoUa_7fd912b0"
 # kubectl get endpoints -n grafana-monitoring
-serverUrl="http://10.244.0.4:3000"
+# curlpod shows different ips >>> service ip and the pod ip are different target the service in curl as the pods go up and down
+serverUrl="http://grafana.grafana-monitoring.svc.cluster.local:3000"
 panelId="1"
 from="2024-01-01T00:00:00.000Z"
 to="2024-01-03T00:00:00.000Z"
