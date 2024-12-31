2d236fbb38daba13a0a85c2b191dcdd9429ce938
Erdem <rifaterdemsahin@gmail.com>
2024-12-31 15:56:16 +0000

    fix the systems update

--- 6_Symbols/2_Grafana/render_dashboard.sh
+++ 6_Symbols/2_Grafana/render_dashboard.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
+# /workspaces/grafana-automation/6_Symbols/2_Grafana/render_dashboard.sh
 
 # Define parameters
 dashboardId="ee8l7vngt74lcc"
 tokenId="glsa_A5cBg7AnfN31RcsUnYRY4ZOVYfTsKoUa_7fd912b0"
-# kubectl get endpoints -n grafana-monitoring
-# curlpod shows different ips >>> service ip and the pod ip are different target the service in curl as the pods go up and down
-# get service endpoint for this
-serverUrl="https://automatic-carnival-46vx5p9qw63qg7x-3000.app.github.dev"
+# minikube ip to send the query to a port forwarded enviroment from the codespaces terminal!
+serverUrl="http://192.168.49.2:3000"
 panelId="1"
 from="2024-01-01T00:00:00.000Z"
 to="2024-01-03T00:00:00.000Z"
